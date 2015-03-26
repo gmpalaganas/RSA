22d8fe198dcfed0cbb0df7d4f5165f133a78b6da
Genesis Palaganas <itetsky123@gmail.com>
2015-03-27 00:21:33 +0800

    Initial Commit

---
+++ rsa.h
@@ -0,0 +1,14 @@
+/*****************************
+*                            *
+*  Genesis Ian M. Palaganas  *
+*  2012-32936                *
+*                            *
+*****************************/
+
+#include <stdio.h>
+
+unsigned long getPhi(unsigned long p, unsigned long q){
+
+	return (p - 1) * (q - 1);
+
+}
